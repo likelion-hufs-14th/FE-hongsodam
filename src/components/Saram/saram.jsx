16b371e040827c55049a846b8a1ae42d1244fa57
qnpdpdpdpfr <amollohaji@gmail.com>
2026-04-06 15:53:57 +0900

    ok

--- src/components/Saram/saram.jsx
+++ src/components/Saram/saram.jsx
@@ -1,26 +1,29 @@
 import './saram.css'
 
+
+function UserItem() {
+  return (
+    <div className='user-item'>
+      <div className='user-photo'>👤</div>
+      <div className='user-info'>
+        <p className='user-name'>사용자</p>
+        <p className='sogae'>안녕하세요. 이웃추가해주세요.</p>
+      </div>
+    </div>
+  )
+}
+
+
 export default function UserList() {
   return (
-    <>
-        <div className='user-list'>
-        <h4 className='user-list-title'>멤버 목록</h4>
-        
-        <div className='user-item'>
-            <div className='user-photo'>👤</div>
-            <p className='user-name'>사용자 1</p>
-        </div>
-
-        <div className='user-item'>
-            <div className='user-photo'>👤</div>
-            <p className='user-name'>사용자 2</p>
-        </div>
-
-        <div className='user-item'>
-            <div className='user-photo'>👤</div>
-            <p className='user-name'>사용자 3</p>
-        </div>
-        </div>
-    </>
+    <div className='user-list'>
+      <h4 className='user-list-title'>멤버 목록</h4>
+      
+      <UserItem />
+      <UserItem />
+      <UserItem />
+      <UserItem />
+      <UserItem />
+    </div>
   )
 }
