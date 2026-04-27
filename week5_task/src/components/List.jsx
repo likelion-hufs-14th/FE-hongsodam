cca4535dc649c995b0ea7ce28fd1369c0fede263
qnpdpdpdpfr <amollohaji@gmail.com>
2026-04-27 17:38:05 +0900

    final

--- week5_task/src/components/List.jsx
+++ week5_task/src/components/List.jsx
@@ -13,11 +13,23 @@ export default function List(props) {
             src={props.data.author.profileImg} 
             style={{ width: '50px', height: '50px', borderRadius: '50%' }} 
           />
-          <p>{props.data.author.name}</p>
+          <span className= 'name'>{props.data.author.name}</span>
+          <span>{props.data.author.role}</span>
         </div>
-          <h3>{props.data.title}</h3>
+          <h3 className= 'title'>{props.data.title}</h3>
           <p>{props.data.content}</p>
-          <p>{props.data.author.role}</p>
+
+        <div className='tags'>
+          {props.data.tags.map((tag, index) => (
+            <span key={index}>
+              #{tag}
+            </span>
+          ))}
+        </div>
+
+          <p style={{ marginRight: '15px' }}>❤️ {props.data.likes}</p>
+          <p className='date'>{props.data.createdAt}</p>
+
       </div>
     </>
   );
